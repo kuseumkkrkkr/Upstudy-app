df77978f0bd2eab76e9b882e597cc01e34a44d81
kuseumkkrkkr <cw20208021@gmail.com>
2026-09-02 02:53:50 +0900

    build(web): deploy course runtime deep links

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "933262953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "684867103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
