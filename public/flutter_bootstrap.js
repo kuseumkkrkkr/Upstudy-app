0eb71c15379f9c0453186725c6c7a609e217b58e
kuseumkkrkkr <cw20208021@gmail.com>
2026-09-11 03:15:21 +0900

    build(student): publish arena html shell

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1607359354" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3196371521" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
