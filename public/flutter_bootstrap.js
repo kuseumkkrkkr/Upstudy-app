a69a5b2db43eb98eb25b0c328062b2bd6f0e0eb9
kuseumkkrkkr <cw20208021@gmail.com>
2026-09-11 12:52:37 +0900

    build: refresh bundle with social parity

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3452737724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "233202514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
