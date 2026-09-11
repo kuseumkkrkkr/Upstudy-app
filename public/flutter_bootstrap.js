a3faf237844a77ffc1536118824d01771e1123af
kuseumkkrkkr <cw20208021@gmail.com>
2026-09-11 12:43:39 +0900

    build: refresh web bundle after parity fixes

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3163332111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3452737724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
